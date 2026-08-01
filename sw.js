1fae02af63eb565550b755c38b315d9c102c9ab0
safsal213 <haay.yo@gmail.com>
2026-08-01 22:54:53 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'what-to-eat-v17-dynamic-background';
+const CACHE_NAME = 'what-to-eat-v18-layout-polish';
 const STATIC_ASSETS = [
   './','index.html','style.css','manifest.json','js/app.js','js/api.js','js/data.js','js/storage.js','js/ui.js',
   'js/swipe.js',
